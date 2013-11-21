f9511a5aee692515269a169fea2d180481552d17
Mattia Basaglia <mattia.basaglia@gmail.com>
2013-11-21 11:07:27 +0100

    Cup help

---
+++ cuphelp.php
@@ -0,0 +1,148 @@
+<?php
+
+const ANYONE= 0;
+const ADMIN = 1;
+const OWNER = 2;
+$help = array(
+    
+    'admin_score'=>array(
+        'auth'=>ADMIN,
+        'synopsis'=>'score match_id score_1 score_2',
+        'desc'=>'Appends the given scores to the score list in the given match',
+    ),
+    'end'=>array(
+        'auth'=>ADMIN,
+        'synopsis'=>'end match_id',
+        'desc'=>'End the given match and save score changes to challonge',
+    ),
+    'admin_cup'=>array(
+        'auth'=>ADMIN,
+        'synopsis'=>'cup cup_name|cup_id',
+        'desc'=>'Change the current cup',
+    ),
+    'cups'=>array(
+        'auth'=>ADMIN,
+        'synopsis'=>'cups',
+        'desc'=>'Show the available cups',
+    ),
+    'admin_maps'=>array(
+        'auth'=>ADMIN,
+        'synopsis'=>'maps +|add|-|remove map_name',
+        'desc'=>'Add or remove a map from the current cup',
+    ),
+    'admin_description'=>array(
+        'auth'=>ADMIN,
+        'synopsis'=>'description new_description',
+        'desc'=>'Change the description for the current cup',
+    ),
+    'start'=>array(
+        'auth'=>ADMIN,
+        'synopsis'=>'start',
+        'desc'=>'Start the cup',
+    ),
+    'admin_time'=>array(
+        'auth'=>ADMIN,
+        'synopsis'=>'time time',
+        'desc'=>'Change the scheduled start time for the current cup',
+    ),
+    'pick'=>array(
+        'auth'=>ADMIN,
+        'synopsis'=>'pick match_id',
+        'desc'=>'Start a map picking session',
+    ),
+    
+    'next'=>array(
+        'auth'=>ANYONE,
+        'synopsis'=>'next [n]',
+        'desc'=>'Show the next (n) scheduled matches',
+    ),
+    'score'=>array(
+        'auth'=>ANYONE,
+        'synopsis'=>'score match_id',
+        'desc'=>'Show the scores for match_id',
+    ),
+    'results'=>array(
+        'auth'=>ANYONE,
+        'synopsis'=>'results',
+        'desc'=>'Show an URL where you can view the cup details',
+    ),
+    'cup'=>array(
+        'auth'=>ANYONE,
+        'synopsis'=>'cup',
+        'desc'=>'Show the current cup name and ID',
+    ),
+    'maps'=>array(
+        'auth'=>ANYONE,
+        'synopsis'=>'maps',
+        'desc'=>'Show the map list for the current cup',
+    ),
+    'description'=>array(
+        'auth'=>ANYONE,
+        'synopsis'=>'description',
+        'desc'=>'Show the description of the current cup',
+    ),
+    'time'=>array(
+        'auth'=>ANYONE,
+        'synopsis'=>'time',
+        'desc'=>'Display the scheduled start time for the current cup',
+    ),
+    
+    'help'=>array(
+        'auth'=>ANYONE,
+        'synopsis'=>'help [command]',
+        'desc'=>'Guess what this does...',
+    ),
+    'noblacklist'=>array(
+        'auth'=>ADMIN,
+        'synopsis'=>'noblacklist User',
+        'desc'=>'Remove user from blacklist',
+    ),
+    'blacklist'=>array(
+        'auth'=>ADMIN,
+        'synopsis'=>'blacklist User',
+        'desc'=>'Add user to blacklist',
+    ),
+    'nowhitelist'=>array(
+        'auth'=>ADMIN,
+        'synopsis'=>'nowhitelist User',
+        'desc'=>'Remove user from whitelist',
+    ),
+    'whitelist'=>array(
+        'auth'=>ADMIN,
+        'synopsis'=>'blacklist User',
+        'desc'=>'Add user to whitelist',
+    ),
+    /*'nick'=>array(
+        'auth'=>OWNER,
+        'synopsis'=>'nick Nickname',
+        'desc'=>'Change nickname',
+    ),
+    'join'=>array(
+        'auth'=>OWNER,
+        'synopsis'=>'join #Channel',
+        'desc'=>'Make the bot join a channel',
+    ),
+    'part'=>array(
+        'auth'=>OWNER,
+        'synopsis'=>'part [#Channel]',
+        'desc'=>'Make the bot part the current channel or the one specified',
+    ),*/
+    'restart'=>array(
+        'auth'=>OWNER,
+        'synopsis'=>'restart',
+        'desc'=>'Restart the bot (quit and rejoin)',
+    ),
+    'quit'=>array(
+        'auth'=>OWNER,
+        'synopsis'=>'quit',
+        'desc'=>'Shut down the bot',
+    ),
+    /*'chanhax'=>array(
+        'auth'=>OWNER,
+        'synopsis'=>'[command] chanhax #Channel',
+        'desc'=>'Execute the command on the given channel',
+    ),*/
+);
+
+
+
