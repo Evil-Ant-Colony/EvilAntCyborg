f9e66b2d6dc0b72ab29e2a5b68993e557c6710a2
Mattia Basaglia <mattia.basaglia@gmail.com>
2014-09-06 23:06:31 +0200

    rcon_data

--- rcon/executors/rcon-extra.php
+++ rcon/executors/rcon-extra.php
@@ -199,7 +199,7 @@ class Rcon2Irc_NotifyAdmin_Troll extends Rcon2Irc_Executor
         if ( $punish )
         {
             $slot = null;
-            foreach ( $rcon_data->player->all() as $player )
+            foreach ( $this->data($data)->player->all() as $player )
             {
                 if ( $player->name == $cmd->params[1] )
                 {
